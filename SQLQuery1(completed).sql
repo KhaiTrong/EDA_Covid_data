13086a1140519108baf406f1df541123be82aa8d
KhaiTrong <125301325+KhaiTrong@users.noreply.github.com>
2023-02-13 17:57:00 +0000

    Delete SQLQuery1(completed).sql

--- SQLQuery1(completed).sql
+++
@@ -1,8 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [continent]
-      ,[location]
-      ,[date]
-      ,[population]
-      ,[new_vaccinations]
-      ,[rolling_people_percentage]
-  FROM [Porftolioproject].[dbo].[rolling_people_percentage]
